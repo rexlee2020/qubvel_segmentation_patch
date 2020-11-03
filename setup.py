7fbedf80643c5df0948b2bb8c09d75b2ada738e6
rexlee2020 <rexlee2001@gmail.com>
2020-11-03 17:25:45 +0900

    sixth

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qubvel_segmentation_patch',
+    version='0.0.1',
+    description='My private pach',
+    url='https://github.com/rexlee2020/qubvel_segmentation_patch.git',
+    author='jongmin lee',
+    author_email='darkize@naver.com',
+    license='unlicense',
+    zip_safe=False
+)
